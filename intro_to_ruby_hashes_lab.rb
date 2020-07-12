c6b63330230ba582e24077a1f52dce5cd405be5d
Dani Bassett <dani@danithetallgirl.com>
2020-07-12 23:48:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,17 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	railroads: {pieces: "4", rent_in_dollars: }
+	railroads: {}
   }
 end
+
+  #{
+   # "Jon Snow" => {
+    # :name=>"Jon",
+    # :email=>"jon_snow@thewall.we",
+    # :favorite_ice_cream_flavors=>["chocolate", "vanilla", "mint chip"],
+    # :address=>"The Lord Commander's Rooms, The Wall, Westeros"
+    #}
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
